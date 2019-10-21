a36211c138f4621776418a59864ab236874d839c
bstojanovski <bojan.stojanovski@comeon.com>
2019-10-21 22:15:38 +0200

    Replaced URL with cors-anywhere link

--- src/components/Search.js
+++ src/components/Search.js
@@ -12,7 +12,7 @@ class Search extends React.Component {
     }
   
     componentDidMount() {
-      fetch("/9.20.0/Api/search/timetable", {
+      fetch("https://cors-anywhere.herokuapp.com/https://be.wizzair.com/9.20.0/Api/search/timetable", {
         method: 'POST',
         headers: {
             'Content-Type': "application/json"
